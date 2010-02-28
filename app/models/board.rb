6f441cbe307914a98e65b7d797a940cacefba1ff
Jakub Kuźma <qoobaa+github@gmail.com>
2010-02-28 16:55:11 +0100

    refactored card model

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,75 +1,7 @@
 class Board < ActiveRecord::Base
   %w(n e s w).each { |d| belongs_to "user_#{d}", :class_name => "User", :extend => UserBoardExtension }
   has_many :bids, :order => "bids.position", :extend => BidsBoardExtension
-  has_many :cards, :order => "cards.position" do
-    def trick(n)
-      where(:position => ((n - 1) * 4 + 1)...(n * 4 + 1)).all
-    end
-
-    def tricks
-      in_groups_of(4, false)
-    end
-
-    # Count completed tricks
-    # TODO: test
-    def completed_tricks_count
-      count.div(4)
-    end
-
-    def current_position
-      count + 1
-    end
-
-    def current_lead_position
-      current_position - (current_position - 1) % 4
-    end
-
-    def previous_lead_position
-      current_lead_position - 4
-    end
-
-    def current_lead?
-      current_position == current_lead_position
-    end
-
-    def previous_lead
-      where(:position => previous_lead_position).first
-    end
-
-    def current_lead
-      where(:position => current_lead_position).first
-    end
-
-    def current_trick_suit
-      current_lead && current_lead.suit
-    end
-
-    def current_trick
-      where(:position => current_lead_position...(current_lead_position + 4))
-    end
-
-    def previous_trick
-      where(:position => previous_lead_position...(previous_lead_position + 4))
-    end
-
-    def previous_trick_suit
-      previous_lead && previous_lead.suit
-    end
-
-    def previous_trick_winner
-      return if previous_trick.empty?
-      card = Bridge::Trick.new(previous_trick.map(&:card)).winner(proxy_owner.trump)
-      proxy_owner.card_owner(card)
-    end
-
-    def current_user
-      if current_lead?
-        previous_trick_winner || proxy_owner.first_lead_user
-      else
-        proxy_owner.card_owner(last.card).next
-      end
-    end
-  end
+  has_many :cards, :order => "cards.position", :extend => CardsBoardExtension
 
   delegate :n, :e, :s, :w, :owner, :to => :deal, :prefix => true, :allow_nil => true
 
