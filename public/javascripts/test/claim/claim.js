1e261d237dc897bd258ce2a77698e85512ba612c
Jakub Kuźma <qoobaa@gmail.com>
2010-05-07 18:32:45 +0200

    added basic test suite for claim widget

--- public/javascripts/test/claim/claim.js
+++ public/javascripts/test/claim/claim.js
@@ -15,22 +15,81 @@ YUI({ filter: "raw" }).use("test", "console", "node-event-simulate", "widget", "
         },
 
         tearDown: function() {
-            // this.claim.destroy();
-            // delete this.claim;
+            this.claim.destroy();
+            delete this.claim;
         },
 
-        testFirst: function() {
+        testMaxTricksChangesTricks: function() {
             this.claim.on("claim", function(event) {
-                Y.log("claim");
-                Y.log(event[0]);
-                Y.log(event[1]);
-            });
+                this.resume(function() {
+                    areSame(5, event[0]);
+                });
+            }, this);
 
+            this.claim.set("maxTricks", 5);
+            Y.one(".yui3-claim-buttons-claim").simulate("click");
+            this.wait();
+        },
+
+        testCantDeclareMoreThanMaxTricks: function() {
+            this.claim.on("claim", function(event) {
+                this.resume(function() {
+                    areSame(13, event[0]);
+                });
+            }, this);
+
+            Y.one(".yui3-claim-more-button").simulate("click");
+            Y.one(".yui3-claim-buttons-claim").simulate("click");
+            this.wait();
+        },
+
+        testDeclareOneTrickLess: function() {
+            this.claim.on("claim", function(event) {
+                this.resume(function() {
+                    areSame(12, event[0]);
+                });
+            }, this);
+
+            Y.one(".yui3-claim-less-button").simulate("click");
+            Y.one(".yui3-claim-buttons-claim").simulate("click");
+            this.wait();
+        },
+
+        testCantDeclareLessThanZeroTricks: function() {
+            this.claim.on("claim", function(event) {
+                this.resume(function() {
+                    areSame(0, event[0]);
+                });
+            }, this);
+
+            for(var i = 0; i <= 20; i++) {
+                Y.one(".yui3-claim-less-button").simulate("click");
+            }
+
+            Y.one(".yui3-claim-buttons-claim").simulate("click");
+            this.wait();
+        },
+
+        testClaimHasExplanation: function() {
+            this.claim.on("claim", function(event) {
+                this.resume(function() {
+                    areSame("explanation", event[1]);
+                });
+            }, this);
+
+            Y.one(".yui3-claim-explanation-input").set("value", "explanation");
+            Y.one(".yui3-claim-buttons-claim").simulate("click");
+            this.wait();
+        },
+
+        testCancel: function() {
             this.claim.on("cancel", function(event) {
-                Y.log("cancel");
-            });
-        }
+                this.resume();
+            }, this);
 
+            Y.one(".yui3-claim-buttons-cancel").simulate("click");
+            this.wait();
+        }
     });
 
     new Y.Console({ newestOnTop: false, width: 500, height: 500 }).render();
