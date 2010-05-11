f7405654372efdfab473efb2852f21884df62d34
Jakub Kuźma <qoobaa+github@gmail.com>
2010-05-11 10:37:23 +0200

    added uiSetText stub

--- public/javascripts/bridge/claimpreview/claimpreview.js
+++ public/javascripts/bridge/claimpreview/claimpreview.js
@@ -27,7 +27,15 @@ YUI.add("claimpreview", function(Y) {
                 contentBox = this.get("contentBox");
 
             html = Y.mustache(ClaimPreview.CLAIM_PREVIEW_TEMPLATE, {
-
+                labelsCN:      this.getClassName("labels"),
+                nameCN:        this.getClassName("name"),
+                tricksCN:      this.getClassName("tricks"),
+                totalCN:       this.getClassName("total"),
+                explanationCN: this.getClassName("explanation"),
+                buttonsCN:     this.getClassName("buttons"),
+                acceptCN:      this.getClassName("accept"),
+                rejectCN:      this.getClassName("reject"),
+                cancelCN:      this.getClassName("cancel")
             });
 
             contentBox.set("innerHTML", html);
@@ -36,7 +44,43 @@ YUI.add("claimpreview", function(Y) {
         bindUI: function() {
             var contentBox = this.get("contentBox");
 
+            this.after("nameChange",          this._afterNameChange);
+            this.after("tricksChange",        this._afterTricksChange);
+            this.after("totalChange",         this._afterTotalChange);
+            this.after("explanationChange",   this._afterExplanationChange);
+            this.after("acceptEnabledChange", this._afterAcceptEnabledChange);
+            this.after("rejectEnabledChange", this._afterRejectEnabledChange);
+            this.after("cancelEnabledChange", this._afterCancelEnabledChange);
+
             contentBox.delegate("click", Y.bind(this._onButtonClick, this), "button[data-event]");
+        },
+
+        _afterNameChange: function(event) {
+            this._uiSetText("." + this.getClassName("name"), event.newVal);
+        },
+
+        _afterTricksChange: function(event) {
+            this._uiSetText("." + this.getClassName("tricks"), event.newVal);
+        },
+
+        _afterTotalChange: function(event) {
+            this._uiSetText("." + this.getClassName("total"), event.newVal);
+        },
+
+        _afterExplanationChange: function(event) {
+            this._uiSetText("." + this.getClassName("explanation"), event.newVal);
+        },
+
+        _afterAcceptEnabledChange: function(event) {
+            this.uiToggleButton("accept", event.newVal);
+        },
+
+        _afterRejectEnabledChange: function(event) {
+            this.uiToggleButton("reject", event.newVal);
+        },
+
+        _afterCancelEnabledChange: function(event) {
+            this.uiToggleButton("cancel", event.newVal);
         },
 
         _onButtonClick: function(event) {
@@ -48,6 +92,19 @@ YUI.add("claimpreview", function(Y) {
 
         syncUI: function() {
 
+        },
+
+        _uiSetText: function(node, value) {
+            var textNode, textTemplate, html,
+                replacements = {},
+                strings = this.get("strings"),
+                contentBox = this.get("contentBox");
+            textNode = contentBox.one(node);
+            textTemplate = strings[text + "Template"] || ("{" + text + "}");
+            replacements[text] = value;
+
+            html = Y.substitute(textTemplate, replacements);
+            textNode.set("innerHTML", html);
         },
 
         _enableButton: function(node) {
@@ -72,22 +129,48 @@ YUI.add("claimpreview", function(Y) {
 
             },
 
+            name: {
+
+            },
+
             tricks: {
 
             },
 
-            name: {
+            total: {
 
+            },
+
+            explanation: {
+
+            },
+
+            acceptEnabled: {
+                validator: Y.Lang.isBoolean
+            },
+
+            rejectEnabled: {
+                validator: Y.Lang.isBoolean
+            },
+
+            cancelEnabled: {
+                validator: Y.Lang.isBoolean
             }
 
         },
 
         CLAIM_PREVIEW_TEMPLATE: ''
             + '<ul>'
+            +   '<li class="{{labelsCN}}">'
+            +     '<span class="{{nameCN}}"></span>'
+            +     '<span class="{{tricksCN}}"></span>'
+            +     '<span class="{{totalCN}}"></span>'
+            +     '<span class="{{explanationCN}}"></span>'
+            +   '</li>'
             +   '<li class="{{buttonsCN}}">'
-            +     '<button type="button" class="{{buttonsAcceptCN}}" data-event="accept">Accept</button>'
-            +     '<button type="button" class="{{buttonsRejectCN}}" data-event="reject">Reject</button>'
-            +     '<button type="button" class="{{buttonsCancelCN}}" data-event="reject">Cancel</button>'
+            +     '<button type="button" class="{{acceptCN}}" data-event="accept">Accept</button>'
+            +     '<button type="button" class="{{rejectCN}}" data-event="reject">Reject</button>'
+            +     '<button type="button" class="{{cancelCN}}" data-event="reject">Cancel</button>'
             +   '</li>'
             + '</ul>'
 
