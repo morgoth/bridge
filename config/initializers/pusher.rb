570e51efd4c5a68acb35665dc47ff0bc08473922
Jakub Kuźma <qoobaa@gmail.com>
2010-08-02 17:49:41 +0200

    added namespace to env variables

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,3 +1,3 @@
-Pusher.app_id = ENV["PUSHER_APP_ID"]
-Pusher.key    = ENV["PUSHER_KEY"]
-Pusher.secret = ENV["PUSHER_SECRET"]
+Pusher.app_id = ENV["BRIDGE_PUSHER_APP_ID"]
+Pusher.key    = ENV["BRIDGE_PUSHER_KEY"]
+Pusher.secret = ENV["BRIDGE_PUSHER_SECRET"]
