1b5725c71e0eb7f868b25d55206c007583838090
Jakub Kuźma <qoobaa@gmail.com>
2010-08-08 17:05:19 +0200

    removed benchmark

--- app/responders/ajax_responder.rb
+++ app/responders/ajax_responder.rb
@@ -1,5 +1,3 @@
-require "benchmark"
-
 class AjaxResponder < ActionController::Responder
   def api_behavior(error)
     if get?
@@ -13,26 +11,16 @@ class AjaxResponder < ActionController::Responder
       current_player = table.players.for(current_user)
       serialized_table = {}
 
-      puts "RENDERING TIME"
-      Benchmark.bm do |x|
-        x.report do
-          serialized_table[nil] = controller.render_to_string(:template => "ajax/tables/show", :locals => {:serializer => serializer, :user => nil})
-          serialized_table["N"] = controller.render_to_string(:template => "ajax/tables/show", :locals => {:serializer => serializer, :user => table.players.n.user}) if table.players.n
-          serialized_table["E"] = controller.render_to_string(:template => "ajax/tables/show", :locals => {:serializer => serializer, :user => table.players.e.user}) if table.players.e
-          serialized_table["S"] = controller.render_to_string(:template => "ajax/tables/show", :locals => {:serializer => serializer, :user => table.players.s.user}) if table.players.s
-          serialized_table["W"] = controller.render_to_string(:template => "ajax/tables/show", :locals => {:serializer => serializer, :user => table.players.w.user}) if table.players.w
-        end
-      end
+      serialized_table[nil] = controller.render_to_string(:template => "ajax/tables/show", :locals => {:serializer => serializer, :user => nil})
+      serialized_table["N"] = controller.render_to_string(:template => "ajax/tables/show", :locals => {:serializer => serializer, :user => table.players.n.user}) if table.players.n
+      serialized_table["E"] = controller.render_to_string(:template => "ajax/tables/show", :locals => {:serializer => serializer, :user => table.players.e.user}) if table.players.e
+      serialized_table["S"] = controller.render_to_string(:template => "ajax/tables/show", :locals => {:serializer => serializer, :user => table.players.s.user}) if table.players.s
+      serialized_table["W"] = controller.render_to_string(:template => "ajax/tables/show", :locals => {:serializer => serializer, :user => table.players.w.user}) if table.players.w
 
-      puts "PUSHER TIME"
-      Benchmark.bm do |x|
-        x.report do
-          Pusher[controller.channel_name(table, nil)].trigger("update-table-data", serialized_table[nil])
+      Pusher[controller.channel_name(table, nil)].trigger("update-table-data", serialized_table[nil])
 
-          (%w[N E S W] - [current_player.try(:direction)]).compact.each do |direction|
-            Pusher[controller.channel_name(table, table.players[direction].try(:user))].trigger("update-table-data", serialized_table[direction]) if serialized_table[direction]
-          end
-        end
+      (%w[N E S W] - [current_player.try(:direction)]).compact.each do |direction|
+        Pusher[controller.channel_name(table, table.players[direction].try(:user))].trigger("update-table-data", serialized_table[direction]) if serialized_table[direction]
       end
 
       render :text => serialized_table[current_player.try(:direction)]
