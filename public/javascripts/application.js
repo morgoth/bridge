a14962926e3098f860b0cb1cbaf585ed5ab605d2
Jakub Kuźma <qoobaa@gmail.com>
2010-09-03 20:08:48 +0200

    added simple image prefetching

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,8 @@
+// prefetch following images (quick fix)
+new Image().src = "/images/cards/deck.png";
+new Image().src = "/images/cards/lost.png";
+new Image().src = "/images/cards/won.png";
+
 YUI(YUICONFIG).use("node", "table", "instantaction", function(Y) {
     if(Y.one("#table")) {
         new Y.Bridge.Table({ container: "#table", plugins: [{ fn: Y.Bridge.InstantAction }] });
