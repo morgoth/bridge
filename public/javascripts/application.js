007eb86bc826a559fe3646803aff509522f777be
Jakub Kuźma <qoobaa+github@gmail.com>
2010-09-18 19:23:18 +0200

    fixed typo

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,4 +1,4 @@
-rYUI(YUICONFIG).use("node", "table", "instantaction", function(Y) {
+YUI(YUICONFIG).use("node", "table", "instantaction", function(Y) {
     if(Y.one("#table")) {
         new Y.Bridge.Table({ container: "#table", plugins: [{ fn: Y.Bridge.InstantAction }] });
     }
