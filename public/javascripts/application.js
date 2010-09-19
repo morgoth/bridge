a8290ec9ceb078d83a261aa7f64cd7877e5d2ae5
Jakub Kuźma <qoobaa+github@gmail.com>
2010-09-19 22:05:54 +0200

    removed unused var in application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,5 @@
 YUI(YUICONFIG).use("node", "table", "instantaction", "gallery-preload", function(Y) {
     if(Y.one("#table")) {
-        var images = "cj,cq,ck,dj,dq,dk,hj,hq,hk,sj,sq,sk";
         new Y.Bridge.Table({ container: "#table", plugins: [{ fn: Y.Bridge.InstantAction }] });
 
         Y.each("cj,cq,ck,dj,dq,dk,hj,hq,hk,sj,sq,sk".split(","), function(card) {
