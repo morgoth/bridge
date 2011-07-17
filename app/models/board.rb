508b1f621ad2acb3980008859066b72180b2684c
Jakub Kuźma <qoobaa@gmail.com>
2011-07-17 12:12:03 +0200

    reformatted code

--- app/models/board.rb
+++ app/models/board.rb
@@ -23,7 +23,7 @@ class Board < ActiveRecord::Base
   %w[n e s w].each do |d|
     define_method("user_#{d}_with_direction") do
       user_n_without_direction.tap do |user|
-        user && user.define_singleton_method("direction") { d.upcase }
+        user && user.define_singleton_method(:direction) { d.upcase }
       end
     end
     alias_method_chain "user_#{d}", :direction
