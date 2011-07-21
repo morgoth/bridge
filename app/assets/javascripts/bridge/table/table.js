8b2f9b586b876b8ed432acefe4492b364fb4ebbc
Jakub Kuźma <qoobaa@gmail.com>
2011-07-21 11:37:35 +0200

    removed initial contract from bidding box

--- app/assets/javascripts/bridge/table/table.js
+++ app/assets/javascripts/bridge/table/table.js
@@ -38,7 +38,7 @@ YUI.add("table", function (Y) {
         },
 
         _renderBiddingBox: function () {
-            this._biddingBox = new Y.Bridge.BiddingBox({ contract: "1C" }).render(this._brNode);
+            this._biddingBox = new Y.Bridge.BiddingBox({}).render(this._brNode);
         },
 
         _renderAuction: function () {
