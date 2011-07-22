577db2a4d4936f8ba7465b0b60cc3353278a1277
biern <biern.m@gmail.com>
2011-07-22 18:28:55 +0200

    added Tricks to playground

--- app/assets/javascripts/playground.js
+++ app/assets/javascripts/playground.js
@@ -1,10 +1,19 @@
-YUI().use("hand", "trick", "biddingbox", "auction", "table-model", function (Y) {
+YUI().use("hand", "trick", "tricks", "biddingbox", "auction", "table-model", function (Y) {
     this.Y = Y;
 
     var cards = ["SA", "SK", "SQ", "SJ", "ST", "S9", "S8", "S7", "S6", "S5", "S4", "S3", "S2",
                  "HA", "HK", "HQ", "HJ", "HT", "H9", "H8", "H7", "H6", "H5", "H4", "H3", "H2",
                  "DA", "DK", "DQ", "DJ", "DT", "D9", "D8", "D7", "D6", "D5", "D4", "D3", "D2",
                  "CA", "CK", "CQ", "CJ", "CT", "C9", "C8", "C7", "C6", "C5", "C4", "C3", "C2"];
+
+    window.tricks = new Y.Bridge.Tricks({ player: "N" }).render();
+    // Add sample tricks
+    for (var i = 0; i < 10; i++) {
+        var f = function(i){
+            var winner = "NESW"[i % 4];
+            window.tricks.addTrick({ get: function () { return winner; }});
+        }(i);
+    }
 
     window.biddingBox = new Y.Bridge.BiddingBox({ contract: "2D", ours: false }).render();
 
